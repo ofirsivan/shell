d8e4aaa900a1b877c7705c4ec81df0fdbf4f8640
Ofir <ofir.sivan@mail.utoronto.ca>
2025-10-08 01:42:16 -0400

    y

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -38,9 +38,7 @@ mkdir data/processed
 cd data/processed
 mkdir server_logs user_logs event_logs
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-cd data/raw
-ls *server*.log
-cp *server*.log data/processed/server_logs
+cp /path/to/data/raw/*server*.log /path/to/destindata/processed/server_logsation/
 # 6. Repeat the above step for user logs and event logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
