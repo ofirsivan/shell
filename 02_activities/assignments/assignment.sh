7d10460b57050f8b1d67a750b66e9bf9736948ef
Ofir <ofir.sivan@mail.utoronto.ca>
2025-10-08 22:34:48 -0400

    ofir

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -49,7 +49,7 @@ rm processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
 cd .. 
 touch data/inventory.txt
-ls -R data/processed > inventory.txt
+ls -R data/processed > data/inventory.txt
 ###########################################
 
 echo "Project setup is complete!"
